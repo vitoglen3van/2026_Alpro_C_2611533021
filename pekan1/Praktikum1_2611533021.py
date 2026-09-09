1de52a02858944cb6278c69ae493287bd6b37cf5
vitoglen3van <vitoglen3@gmail.com>
2026-09-09 12:03:50 +0700

    Add files via upload

---
+++ pekan1/Praktikum1_2611533021.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Departemen Informatika")
+print("Universitas Andalas")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini " + "dibuat oleh " 
+ + "Vito Glenevan_2611533021")
