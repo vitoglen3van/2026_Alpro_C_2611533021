3ce7a9131672226aaaec70ee8a9911269891986a
vitoglen3van <vitoglen3@gmail.com>
2026-09-10 12:02:42 +0700

    Add files via upload

---
+++ Pekan2/Vito Input_2611533021.py
@@ -0,0 +1,4 @@
+panjang_3021 = input('masukkan panjang')
+lebar_3021 = input('masukkan lebar')
+luas_3021 = int(panjang_3021) * int(lebar_3021)
+print("Luas =, luas_3021")
---
+++ Pekan2/Vito String_2611533021.py
@@ -0,0 +1,12 @@
+nama = "Vito_3021"
+jenis_kelamin = 'L'
+alamat = """
+Kampus Unand
+kec.kuranji
+kota Padang
+"""
+agama = 'islam'
+print("Nama saya adalah: ", nama)
+print("Jenis kelamin saya adalah: ", jenis_kelamin)
+print("Alamat saya adalah: ", alamat)
+print("Agama saya adalah: ", agama)
