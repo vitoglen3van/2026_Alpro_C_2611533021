3bf4a9ad8efbb2584f21eb43b32d25e92a7be28d
vitoglen3van <vitoglen3@gmail.com>
2026-09-12 23:10:15 +0700

    Delete Pekan2 directory

--- Pekan2/Vito Input_2611533021.py
+++
@@ -1,4 +0,0 @@
-panjang_3021 = input('masukkan panjang')
-lebar_3021 = input('masukkan lebar')
-luas_3021 = int(panjang_3021) * int(lebar_3021)
-print("Luas =, luas_3021")
--- Pekan2/Vito Konstanta_2611533021.py
+++
@@ -1,6 +0,0 @@
-from typing import Final
-PI: Final = 3.14
-print("pi: %f % (PI)")
-jari_3021 = float(input('8'))
-luas_3021 = PI * jari_3021 * jari_3021
-print("Luas lingkaran dengan jari_jari %.2f adalah %.2f % (jari_3021, luas_3021)")
