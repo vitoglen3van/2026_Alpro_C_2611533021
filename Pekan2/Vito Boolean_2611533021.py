f014d5a0796488dbfa2d95afd8c42a345c56fb4a
vitoglen3van <vitoglen3@gmail.com>
2026-09-12 23:10:59 +0700

    Add files via upload

---
+++ Pekan2/Vito Boolean_2611533021.py
@@ -0,0 +1,13 @@
+is_lulus = True
+is_cumlaude = True
+
+nilai_3021 = 85
+batas_lulus = 75
+
+status_kelulusan = nilai_3021 >= batas_lulus #Hasilnya akan True
+
+print("=== Check Kelulusan ===")
+print("nilai:", nilai_3021)
+print("Apakah Lulus?:", status_kelulusan)
+if is_lulus and is_cumlaude: ''
+print("Selamat, Anda lulus dengan predikat CumLaude!")
---
+++ Pekan2/Vito Numerik_2611533021.py
@@ -0,0 +1,11 @@
+#create a variable with numeric type value.
+a_3021=100
+print("The type of a variable having value", a_3021, " is ", type(a_3021))
+
+#create a variable with float value.
+b_3021=10.2345
+print("The type of variable having value", b_3021, " is ", type(b_3021))
+
+#create a variable with complex value.
+c_3021=100+3j
+print("The type of variable having value", c_3021, " is ", type(c_3021))
