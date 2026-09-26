02dcb6f1b6914b29409a363ffa6a0915e5efff57
vitoglen3van <vitoglen3@gmail.com>
2026-09-26 22:20:46 +0700

    Delete Pekan4 directory

--- Pekan4/If1_2611533021.py
+++
@@ -1,6 +0,0 @@
-ipk_3021 = float(input("Inputkan IPK anda: "))
-
-if ipk_3021 > 2.75:
-    print("Anda Lulus Sangat Memuaskan dengan IPK " + str(ipk_3021))
-
-print("Program Selesai")
--- Pekan4/If2_2611533021.py
+++
@@ -1,7 +0,0 @@
-ipk_3021 = float(input("Input IPK Anda = "))
-
-if ipk_3021 > 2.75:
-    print("Anda Lulus Sangat Memuaskan dengan IPK " + str(ipk_3021))
-else:
-    print("Anda Tidak Lulus")
-print("Program Selesai")
